db974b43ce87255b78f84c361124caa525ec3dee
Satyam Verma <satyam.kumar.verma2002@gmail.com>
2026-08-24 23:09:06 +0530

    add

---
+++ 67.JS_DOM/script.js
@@ -0,0 +1,13 @@
+// READ THIS PAGE FOR FULL INFO
+// https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/JS_Chapterwise_Notes.pdf
+// [PAGE No. 33]
+
+console.log("Hello world")
+
+document.body.firstElementChild 
+
+// in this you will only get element node
+document.body.firstElementChild.childNodes 
+
+// in below this you get all include text node or element node
+document.body.firstElementChild.children
